62d0d5b22ef3d4f229b7777f2eec4e047e770b9f
randrahim <rrahim3@gatech.edu>
2025-01-11 17:06:24 -0500

    Send Back Stuff for Sale

--- backend/app.js
+++ backend/app.js
@@ -2,23 +2,60 @@ const express = require("express");
 
 const app = express();
 
+// app.use((req, res, next) => {
+//   console.log("Request received!");
+//   next();
+// });
+
+// app.use((req, res, next) => {
+//   res.status(201);
+//   next();
+// });
+
+// app.use((req, res, next) => {
+//   res.json({ message: "Your request was successful!" });
+//   next();
+// });
+
+// app.use((req, res, next) => {
+//   console.log("Response sent successfully!");
+// });
+
 app.use((req, res, next) => {
-  console.log("Request received!");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
   next();
 });
 
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
-
-app.use((req, res, next) => {
-  res.json({ message: "Your request was successful!" });
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log("Response sent successfully!");
+app.use("/api/stuff", (req, res, next) => {
+  const stuff = [
+    {
+      _id: "oeihfzeoi",
+      title: "My first thing",
+      description: "All of the info about my first thing",
+      imageUrl:
+        "https://th.bing.com/th?id=OIP.FMwWdTulrv36Wf022uD-NQHaHa&w=250&h=250&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2",
+      price: 4900,
+      userId: "qsomihvqios",
+    },
+    {
+      _id: "oeihfzeomoihi",
+      title: "My second thing",
+      description: "All of the info about my second thing",
+      imageUrl:
+        "https://th.bing.com/th?id=OIP.FMwWdTulrv36Wf022uD-NQHaHa&w=250&h=250&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2",
+      price: 2900,
+      userId: "qsomihvqios",
+    },
+  ];
+  res.status(200).json(stuff);
 });
 
 module.exports = app;
