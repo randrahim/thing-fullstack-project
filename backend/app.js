160df826dcf87769b12a7f1c54c886a412cccf5d
randrahim <rrahim3@gatech.edu>
2025-01-11 17:52:39 -0500

    a code to delete a stuff

--- backend/app.js
+++ backend/app.js
@@ -91,6 +91,20 @@ app.put("/api/stuff/:id", (req, res, next) => {
     });
 });
 
+app.delete("/api/stuff/:id", (req, res, next) => {
+  Thing.deleteOne({ _id: req.params.id })
+    .then(() => {
+      res.status(200).json({
+        message: "Deleted!",
+      });
+    })
+    .catch((error) => {
+      res.status(400).json({
+        error: error,
+      });
+    });
+});
+
 app.get("/api/stuff", (req, res, next) => {
   Thing.find()
     .then((things) => {
